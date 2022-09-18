ac96bf2ab03c53fae2fdf0ce883c9cfb6f812a08
Tim Morgan <git@timothymorgan.info>
2022-09-18 12:53:10 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# AvidyneR9ToGarmin
+
+Converts Avidyne R9 log files to Garmin CSV format, for use with websites that
+expect logs in Garmin format.
+
+## Installation
+
+AvidyneR9ToGarmin is a Swift Package Manager project with an executable target.
+It has been tested on macOS 12 using Swift 5.6, but will likely work on other
+platforms.
+
+1. Edit the `Sources/libAvidyneR9ToGarmin/Converter/R9ToGarminConverter.swift`
+   file. Change the `private static let headers` constant; in particular, the
+   `aircraft_ident` and `system_id` fields. **Note that many websites use these
+   fields to automatically assign log files to aircraft profiles.**
+2. Run `swift build` (or `swift build -c release`) to create the
+   `avidyne-r9-to-garmin` executable.
+
+## Usage
+
+```
+USAGE: avidyne-r9-to-garmin <input> <output> [--verbose]
+
+ARGUMENTS:
+  <input>                 The directory where Avidyne R9 CSVs are stored.
+  <output>                The directory to contain the generated Garmin CSV files.
+
+OPTIONS:
+  --verbose               Include extra information in the output.
+  -h, --help              Show help information.
+```
+
+The executable will create a Garmin log file for each power-on cycle of the R9
+system. Empty log files will automatically be deleted; however, a "long tail" of
+trivially small log files will still be present. You can, at your discretion,
+delete these irrelevant smaller log files.
+
+## Limitations
+
+- The R9 system does not log many of the data points present in Garmin logs
+  (e.g., target vertical speed, GPS _U_/_X_/_W_, wind direction and speed, etc.)
+  Some websites can interpolate some of these values, but others will
+  necessarily be un-derivable.
---
+++ Sources/libAvidyneR9ToGarmin/Converter/Conversion.swift
@@ -0,0 +1,269 @@
+import Foundation
+
+extension R9ToGarminConverter {
+    func r9RecordsToGarminRecord(_ r9Records: Array<R9Record>, date: Date) throws -> GarminRecord {
+        let (engineRecord, flightRecord, systemRecord) = try recordCombiners(from: r9Records, date: date)
+        
+        var baroAltitude: Int? = nil
+        if let altimeterSetting = systemRecord.get(\.altimeterSetting),
+           let pressureAltitude = flightRecord.get(\.pressureAltitude) {
+            baroAltitude = pressureAltitude + Int(((altimeterSetting - 29.92)*1000).rounded()) //TODO is this good enough?
+        }
+        
+        var autopilotOn = false
+        if let latActive = flightRecord.get(\.DFC100_activeLateralMode),
+           let vertActive = flightRecord.get(\.DFC100_activeVerticalMode) {
+            autopilotOn = latActive != 0 && vertActive != 0
+        }
+        
+        let navSource = indexToStr(systemRecord.get(\.courseSelect, strategy: .mode), values: Self.cdiSources)
+        let FMS = navSource == "GPS1"
+        
+        var GPSNavDeviation: Float? = nil
+        if let xdev = systemRecord.get(\.crossTrackDeviation),
+           let FMSMode = systemRecord.get(\.courseSelect, strategy: .mode) {
+            GPSNavDeviation = xdev/Self.fmsCDIScales[Int(FMSMode)]
+        }
+        
+        let latitude = flightRecord.get(\.GPSLatitude)
+        let longitude = flightRecord.get(\.GPSLongitude)
+        let altitudeGPS = m_ft_i(systemRecord.get(\.GPSAltitudeMSL))
+        let GPSFixStatus = indexToStr(systemRecord.get(\.GPSState, strategy: .mode), values: Self.gpsStates)
+        let groundSpeed = to_f(systemRecord.get(\.groundSpeed))
+        let groundTrack = magToTrue(systemRecord.get(\.groundTrack, strategy: .bearing), var: systemRecord.get(\.magneticVariation))
+        let heading = flightRecord.get(\.heading, strategy: .bearing)
+        let pressureAltitude = flightRecord.get(\.pressureAltitude)
+        let verticalSpeed = flightRecord.get(\.verticalSpeed)
+        let indicatedAirspeed = to_f(flightRecord.get(\.indicatedAirspeed))
+        let trueAirspeed = to_f(flightRecord.get(\.trueAirspeed))
+        let pitch = flightRecord.get(\.pitch)
+        let roll = flightRecord.get(\.roll)
+        let lateralAcceleration = flightRecord.get(\.lateralAcceleration)
+        let normalAcceleration = flightRecord.get(\.filteredNormalAcceleration) ?? flightRecord.get(\.normalAcceleration)
+        let headingBug = systemRecord.get(\.headingBug, strategy: .mode)
+        let altitudeBug = systemRecord.get(\.altitudeBug, strategy: .mode)
+        let altimeterSetting = systemRecord.get(\.altimeterSetting, strategy: .mode)
+        
+        let navIdentifier = systemRecord.get(\.activeWaypoint)
+        let navFrequency = kHz_MHz(systemRecord.get(\.navFrequency, strategy: .mode))
+        let navDistance = systemRecord.get(\.distanceToWaypoint)
+        let navBearing = FMS
+            ? systemRecord.get(\.navaidBearing, strategy: .bearing)
+            : nil
+        let navCourse = FMS
+            ? systemRecord.get(\.desiredTrack, strategy: .bearing)
+            : systemRecord.get(\.OBS, strategy: .bearing)
+        let crossTrackDistance = FMS
+            ? systemRecord.get(\.crossTrackDeviation)
+            : nil
+        let horizontalCDIDeflection = FMS
+            ? GPSNavDeviation
+            : systemRecord.get(\.localizerDeviation)
+        let horizontalCDIScale = FMS
+            ? indexToStr(systemRecord.get(\.navigationMode, strategy: .mode), values: Self.fmsModes)
+            : nil
+        let verticalCDIDeflection = FMS
+            ? nil
+            : systemRecord.get(\.glideslopeDeviation)
+        let VNAVTargetAltitude = flightRecord.get(\.DFC100_altitudeTarget, strategy: .mode)
+        let FDLateralMode = autopilotModeStr(active: flightRecord.get(\.DFC100_activeLateralMode, strategy: .mode),
+                                             armed: flightRecord.get(\.DFC100_armedLateralMode, strategy: .mode),
+                                             values: Self.autopilotLateralModes)
+        let FDVerticalMode = autopilotModeStr(active: flightRecord.get(\.DFC100_activeVerticalMode, strategy: .mode),
+                                              armed: flightRecord.get(\.DFC100_armedVerticalMode, strategy: .mode),
+                                              values: Self.autopilotVerticalModes)
+        let FDRollCommand = flightRecord.get(\.FDRoll)
+        let FDPitchCommand = flightRecord.get(\.FDPitch)
+        let APRollCommand = autopilotOn ? flightRecord.get(\.FDRoll) : nil
+        let APPitchCommand = autopilotOn ? flightRecord.get(\.FDPitch) : nil
+        let magneticVariation = systemRecord.get(\.magneticVariation)
+        let outsideAirTemperature = to_f(systemRecord.get(\.oat))
+        let heightAGL = m_ft_i(systemRecord.get(\.GPSHeightAGL))
+        let oilTemperature = engineRecord.get(\.oilTemperature)
+        let oilPressure = engineRecord.get(\.oilPressure)
+        let RPM = engineRecord.get(\.RPM)
+        let manifoldPressure = engineRecord.get(\.manifoldPressure)
+        let potential1 = engineRecord.get(\.mainBus1Potential)
+        let potential2 = engineRecord.get(\.emergencyBusPotential)
+        let fuelFlow = engineRecord.get(\.fuelFlow)
+        let CHTs = engineRecord.get(\.CHTs).map { to_i($0) }
+        let EGTs = engineRecord.get(\.EGTs).map { to_i($0) }
+        let percentPower = to_ui8(engineRecord.get(\.percentPower))
+        
+        return .init(date: date,
+                     latitude: latitude,
+                     longitude: longitude,
+                     altitudeGPS: altitudeGPS,
+                     GPSFixStatus: GPSFixStatus,
+                     groundSpeed: groundSpeed,
+                     groundTrack: groundTrack,
+                     heading: heading,
+                     pressureAltitude: pressureAltitude,
+                     baroAltitude: baroAltitude,
+                     verticalSpeed: verticalSpeed,
+                     indicatedAirspeed: indicatedAirspeed,
+                     trueAirspeed: trueAirspeed,
+                     pitch: pitch,
+                     roll: roll,
+                     lateralAcceleration: lateralAcceleration,
+                     normalAcceleration: normalAcceleration,
+                     headingBug: headingBug,
+                     altitudeBug: altitudeBug,
+                     altimeterSetting: altimeterSetting,
+                     navSource: navSource,
+                     navIdentifier: navIdentifier,
+                     navFrequency: navFrequency,
+                     navDistance: navDistance,
+                     navBearing: navBearing,
+                     navCourse: navCourse,
+                     crossTrackDistance: crossTrackDistance,
+                     horizontalCDIDeflection: horizontalCDIDeflection,
+                     horizontalCDIScale: horizontalCDIScale,
+                     verticalCDIDeflection: verticalCDIDeflection,
+                     VNAVTargetAltitude: VNAVTargetAltitude,
+                     autopilotState: autopilotOn ? "AP" : nil,
+                     FDLateralMode: FDLateralMode,
+                     FDVerticalMode: FDVerticalMode,
+                     FDRollCommand: FDRollCommand,
+                     FDPitchCommand: FDPitchCommand,
+                     APRollCommand: APRollCommand,
+                     APPitchCommand: APPitchCommand,
+                     magneticVariation: magneticVariation,
+                     outsideAirTemperature: outsideAirTemperature,
+                     densityAltitude: nil, //TODO calculate this>
+                     heightAGL: heightAGL,
+                     windSpeed: nil,
+                     windDirection: nil, //TODO calculate these?
+                     oilTemperature: oilTemperature,
+                     oilPressure: oilPressure,
+                     RPM: RPM,
+                     manifoldPressure: manifoldPressure,
+                     potential1: potential1,
+                     potential2: potential2,
+                     fuelFlow: fuelFlow,
+                     CHTs: CHTs,
+                     EGTs: EGTs,
+                     percentPower: percentPower)
+    }
+    
+    private func recordCombiners(from records: Array<R9Record>, date: Date) throws -> (R9RecordCombiner<R9EngineRecord>, R9RecordCombiner<R9FlightRecord>, R9RecordCombiner<R9SystemRecord>) {
+        let engineRecords: Array<R9EngineRecord> = records.filter { $0.type == .engine } as! Array<R9EngineRecord>
+        guard !engineRecords.isEmpty else { throw AvidyneR9ToGarminError.incompleteRecordsForDate(date) }
+        let engineCombiner = R9RecordCombiner(records: engineRecords)
+        
+        let flightRecords: Array<R9FlightRecord> = records.filter { $0.type == .flight } as! Array<R9FlightRecord>
+        guard !flightRecords.isEmpty else { throw AvidyneR9ToGarminError.incompleteRecordsForDate(date) }
+        let flightCombiner = R9RecordCombiner(records: flightRecords)
+        
+        let systemRecords: Array<R9SystemRecord> = records.filter { $0.type == .system } as! Array<R9SystemRecord>
+        guard !systemRecords.isEmpty else { throw AvidyneR9ToGarminError.incompleteRecordsForDate(date) }
+        let systemCombiner = R9RecordCombiner(records: systemRecords)
+        
+        return (engineCombiner, flightCombiner, systemCombiner)
+    }
+    
+    private func m_ft(_ m: Float?) -> Float? {
+        guard let m = m else { return nil }
+        return m * 3.28084
+    }
+    
+    private func m_ft(_ m: Int?) -> Float? {
+        guard let m = m else { return nil }
+        return m_ft(Float(m))
+    }
+    
+    private func m_ft_i(_ m: Float?) -> Int? {
+        guard let ft = m_ft(m) else { return nil }
+        return Int(ft.rounded())
+    }
+    
+    private func m_ft_i(_ m: Int?) -> Int? {
+        guard let m = m else { return nil }
+        guard let ft = m_ft_i(Float(m)) else { return nil }
+        return Int(ft)
+    }
+    
+    private func kHz_MHz(_ kHz: UInt?) -> Float? {
+        guard let kHz = kHz else { return nil }
+        return Float(kHz)/1000
+    }
+    
+    private func to_f(_ i: Int?) -> Float? {
+        guard let i = i else { return nil }
+        return Float(i)
+    }
+    
+    private func to_f(_ i: UInt?) -> Float? {
+        guard let i = i else { return nil }
+        return Float(i)
+    }
+    
+    private func to_i(_ f: Float?) -> Int? {
+        guard let f = f else { return nil }
+        return Int(f.rounded())
+    }
+    
+    private func to_ui8(_ f: Float?) -> UInt8? {
+        guard let f = f else { return nil }
+        return UInt8(f.rounded())
+    }
+    
+    private func magToTrue(_ mag: Int?, `var`: Float?) -> UInt16? {
+        guard let mag = mag, let `var` = `var` else { return nil }
+        let `true` = Int((Float(mag) + `var`).rounded())
+        return normalizedHeading(`true`)
+    }
+    
+    private func magToTrue(_ mag: Float?, `var`: Float?) -> Float? {
+        guard let mag = mag, let `var` = `var` else { return nil }
+        let `true` = Float(mag) + `var`
+        return normalizedHeading(`true`)
+    }
+    
+    private func normalizedHeading(_ heading: Int?) -> UInt16? {
+        guard let heading = heading else { return nil }
+
+        var modHeading = heading % 360
+        if modHeading < 0 { modHeading += 360 }
+        
+        return UInt16(modHeading)
+    }
+    
+    private func normalizedHeading(_ heading: Float?) -> Float? {
+        guard let heading = heading else { return nil }
+        
+        var modHeading = heading.truncatingRemainder(dividingBy: 360)
+        if modHeading < 0 { modHeading += 360 }
+        
+        return modHeading
+    }
+    
+    private func indexToStr(_ index: UInt8?, values: Array<String?>) -> String? {
+        guard let index = index else { return nil }
+        return values[Int(index)]
+    }
+    
+    private static let gpsStates: Array<String?> = ["NoSoln", "NoSoln", "NoSoln", "NoSoln", "3D-", "3D", "3DDiff"]
+    private static let fmsModes: Array<String?> = ["OCN", "ENR", "TERM", "DEP", "GA", "APCH"]
+    private static let fmsCDIScales: Array<Float> = [30380.6, 12152.2, 6076.12, 6076.12, 6076.12, 1822.83]
+    private static let cdiSources: Array<String?> = ["GPS1", "LOC1", "LOC2"]
+    private static let gpsSources: Array<String?> = ["AUTO", "GPS1", "GPS2"]
+    private static let autopilotLateralModes: Array<String?> = [nil, "ROLL", "HDG", "LOC", "LOC-BC", "VOR", "VOR-APPR", "APPR", "NAV", "NAV-INTCPT"]
+    private static let autopilotVerticalModes: Array<String?> = [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "PITCH", "IAS", "VS", "ALT", "GS", "ALT-GS", "VNAV-ALT", "VNAV-VS"]
+    
+    private func autopilotModeStr(active: UInt8?, armed: UInt8?, values: Array<String?>) -> String? {
+        guard let active = active else { return nil }
+        guard let activeStr = values[Int(active)] else { return nil }
+        
+        if let armed = armed {
+            let armedStr = values[Int(armed)]
+            if let armedStr = armedStr {
+                return "\(activeStr) (\(armedStr))"
+            } else {
+                return activeStr
+            }
+        } else {
+            return activeStr
+        }
+    }
+}
