5e91b1212db572f1200d2e3812825b094791679f
Tim Morgan <github@timothymorgan.info>
2024-04-04 00:22:48 -0700

    Swift 5.10; package updates

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AvidyneR9ToGarmin",
     platforms: [.macOS(.v14)],
-    
+
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "libAvidyneR9ToGarmin", targets: ["libAvidyneR9ToGarmin"]),
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/dehesa/CodableCSV.git", from: "0.6.7"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/nicklockwood/VectorMath.git", branch: "master")
     ],
@@ -40,7 +40,7 @@ let package = Package(
             name: "AvidyneR9ToGarminTests",
             dependencies: ["libAvidyneR9ToGarmin"]),
     ],
-    
+
     swiftLanguageVersions: [.v5]
 )
 
