087bf12eb45f3deef8eedda5ad03d42d23cfe7b4
Tim Morgan <github@timothymorgan.info>
2024-12-03 00:19:48 -0800

    Add command configuration

--- Sources/AvidyneR9ToGarmin/AvidyneR9ToGarmin.swift
+++ Sources/AvidyneR9ToGarmin/AvidyneR9ToGarmin.swift
@@ -5,6 +5,16 @@ import libAvidyneR9ToGarmin
 
 @main
 struct AvidyneR9ToGarmin: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Converts Avidyne R9 log files to Garmin CSV format, for use with websites that expect logs in Garmin format.",
+        discussion: """
+            The executable will create a Garmin log file for each power-on cycle of the R9
+            system. Empty log files will automatically be deleted; however, a "long tail" of
+            trivially small log files will still be present. You can, at your discretion,
+            delete these irrelevant smaller log files.
+            """
+    )
+    
     @Argument(help: "The directory where Avidyne R9 CSVs are stored.",
               completion: .directory,
               transform: { URL(filePath: $0, directoryHint: .isDirectory) })
